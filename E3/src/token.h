0d186d82e1fd069c751049706df14e553c0f8d81
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2024-09-07 00:31:46 -0300

    feat: apply correct numbers to all tokens

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,51 +1,32 @@
 /* token.h */
 
-typedef enum {
-        ID = 256, 
-	NUM, 
+typedef enum
+{
+        ID = 256,
+        NUM,
         KEY,
-        // ADD,                      // +
-        // SUBTRACT,                 // -
-        // MULTIPLY,                 // *
-        // DIVIDE,                   // /
-        // MOD,                      // %
-        // NOT,                      // !
-        // LESS,                     // <
-        // GREATER,                  // >
-        // ATTRIBUTE,                // =
-        // COLON,                    // :
-        // SEMICOLON,                // ;
-        // COMMA,                    // ,
-        // OPAREN,                   // (
-        // CPAREN,                   // )
-        // OBRACKET,                 // [
-        // CBRACKET,                 // ]
-        // OBRACE,                   // {
-        // CBRACE,                   // }
-        // BACKSLASH,
-        LEQUAL,                   // <=
-        GEQUAL,                   // >=  
-        KEY_ELSE,
-        EQUAL,                    // ==  
-        NEQUAL,                   // !=  
-        AND,                      // &&  
-        KEY_IF,
-        OR,                       // ||
-        KEY_PRINT,
+        KEY_BOOLEAN,  // 259
+        KEY_CHAR,     // 260
+        KEY_ELSE,     // 261
+        KEY_FALSE,    // 262
+        KEY_FOR,      // 263
+        KEY_FUNCTION, // 264
+        KEY_IF,       // 265
+        KEY_INTEGER,  // 266
+        KEY_PRINT,    // 267
+        KEY_RETURN,   // 268
+        KEY_STRING,   // 269
+        KEY_TRUE,     // 270
+        KEY_VOID,     // 271
+        KEY_WHILE,    // 272
+        LEQUAL,       // 273
+        GEQUAL,       // 274
+        EQUAL,        // 275
+        NEQUAL,       // 276
+        AND,          // 277
+        OR,           // 278
         KEY_ARRAY,
-        KEY_BOOLEAN,
-        KEY_CHAR,
-        KEY_FALSE,
-        KEY_FOR,
-        KEY_FUNCTION,
-        KEY_INTEGER,
-        KEY_RETURN,
-        KEY_STRING,
-        KEY_TRUE,
-        KEY_VOID,
-        KEY_WHILE,
         CHAR,
-	STR,
-	ERROR
-} token_t; 
-
+        STR,
+        ERROR
+} token_t;
